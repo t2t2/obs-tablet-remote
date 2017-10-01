3f92032490d40ed684038022695385b70f1ad4b5
t2t2 <taavot@gmail.com>
2017-10-01 21:45:49 +0300

    Reset layout button

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -5,6 +5,12 @@
 			<button class="close" @click="showSettings(false)"></button>
 		</div>
 
+		<div class="settings-container">
+			<div class="settings-group">
+				<h3>Danger Zone</h3>
+				<button @click="askForReset">Reset Layout</button>
+			</div>
+		</div>
 	</overlay>
 </template>
 
@@ -18,7 +24,13 @@
 			Overlay
 		},
 		methods: {
-			...mapMutations(['showSettings'])
+			askForReset() {
+				if (confirm('Are you sure you want to reset the layout?')) {
+					this.resetLayout()
+				}
+			},
+			...mapMutations(['showSettings']),
+			...mapMutations('layout', ['resetLayout'])
 		}
 	}
 </script>
